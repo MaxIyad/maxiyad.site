15ff84aa396dccb49a37bfe53bdb9a831adae760
MaxIyad <hi@maxiyad.com>
2024-10-17 12:25:17 +0100

    -

--- src/utils/data-utils.ts
+++ src/utils/data-utils.ts
@@ -1,5 +1,5 @@
 import { type CollectionEntry } from 'astro:content';
 
 export function sortItemsByDateDesc(itemA: CollectionEntry<'pages'>, itemB: CollectionEntry<'pages'>) {
-    return new (itemB.data.order) - new (itemA.data.order);
+    return new (itemB.frontmatter.order) - new (itemA.frontmatter.order);
 }
