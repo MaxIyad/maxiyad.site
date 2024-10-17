261ddaa81bd55fac78035801b0a5336928b22268
MaxIyad <hi@maxiyad.com>
2024-10-17 11:48:24 +0100

    -

--- src/utils/data-utils.ts
+++ src/utils/data-utils.ts
@@ -1,6 +1,5 @@
 import { type CollectionEntry } from 'astro:content';
-import { slugify } from './common-utils';
 
-export function sortItemsByDateDesc(itemA: CollectionEntry<'blog' | 'pages'>, itemB: CollectionEntry<'blog' | 'pages'>) {
+export function sortItemsByDateDesc(itemA: CollectionEntry<'pages'>, itemB: CollectionEntry<'pages'>) {
     return new Date(itemB.data.publishDate).getTime() - new Date(itemA.data.publishDate).getTime();
 }
